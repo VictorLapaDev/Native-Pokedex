01eb3126d6fd26bdaf23d5ab78fbd5f6f9497938
VictorLapaDev <victorlucaslapa@gmail.com>
2024-11-02 21:37:48 -0300

    feat: finalized infoPoke

--- src/components/InfoPoke.tsx
+++ src/components/InfoPoke.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Center, HStack, Icon, ImageBackground, Modal, ModalCloseButton, ModalContent } from "@gluestack-ui/themed"
+import { Box, Button, Center, HStack, Icon, ImageBackground, Modal, ModalCloseButton, ModalContent, VStack } from "@gluestack-ui/themed"
 import { ModalBackdrop } from "@gluestack-ui/themed"
 import { Image } from "@gluestack-ui/themed"
 import { ButtonText } from "@gluestack-ui/themed"
@@ -43,6 +43,15 @@ const InfoPoke = ({poke, isModal, setIsModal}: props) => {
         fairy: "#F0B6B6"
     }
 
+    const statsColor = {
+        'HP': '#FF0000', 
+        'ATK': '#FFA500', 
+        'DEF': '#0000FF', 
+        'SP.DEF': '#800080', 
+        'SP.ATK': '#FFC0CB', 
+        'SPD': '#008000' 
+    }
+
 
   return (
 
@@ -57,13 +66,17 @@ const InfoPoke = ({poke, isModal, setIsModal}: props) => {
                 
 
                 <ModalContent w={'100%'} h={'100%'} bgColor="#1E1E1E">
+
                 <ModalHeader borderRadius={10}>
-                    <ImageBackground source={require('../assets/fundoPoke.jpg')} style={{ width: '100%', height: 300 }} >
+                    <ImageBackground source={require('../assets/fundoPoke.jpg')}  style={{ width: '100%', height: 300 }}  >
                         <HStack justifyContent="space-between" p={4}>
-                            <ModalCloseButton>
-                                <Icon as={ArrowLeftIcon} color="$white"/>
-                            </ModalCloseButton>
-                            <Heading size="lg" color="$white" right={10}>{poke.name}</Heading>
+                            <HStack gap={10}>
+                                <ModalCloseButton>
+                                    <Icon as={ArrowLeftIcon} color="$white"/>
+                                </ModalCloseButton>
+                                <Heading size="lg" color="$white" right={10}>{poke.name}</Heading>
+                            </HStack>
+                        <Text color="white">#{poke.id}</Text>
                         </HStack>
 
                         <Image w={'100%'} h={'90%'} source={{uri: shiny? imageShyne : image}} alt={poke.name} resizeMode="contain" />
@@ -71,47 +84,110 @@ const InfoPoke = ({poke, isModal, setIsModal}: props) => {
                     </ImageBackground>
                 </ModalHeader>
 
+                <ModalBody bgColor={"$warmGray300"}>
+
+                    <Center mt={10}>
+                        <Text mb={5} color="black" fontWeight="$normal" fontSize={40}>{poke.name}</Text>
+                        <HStack>
+                            {poke.types.map((type: object, index) => { 
+                                            const tipo = type.type.name;
+                                            const bgTipo = typeColors[tipo] || 'gray';
+                                            return(
+                                                <Center key={index} w={100} borderRadius={20}  ml={3} bgColor={bgTipo}>
+                                                    <Text  padding={10} color="white" fontWeight="$bold" >
+                                                        {tipo}
+                                                    </Text>
+                                                </Center>
+                                            )
+                                            }
+                                        )}
+                        </HStack>
+                    </Center>
 
 
+                    <Center  mt={10}>
+                        <HStack gap={100}>
+                            <VStack alignItems="center">
+                                <Text color="black" fontSize={25}>{(poke.weight)/10} KG</Text>
+                                <Text>Weight</Text>
+                            </VStack>
+                            <VStack alignItems="center">
+                                <Text color="black" fontSize={25}>{(poke.height)/10} M</Text>
+                                <Text>Height</Text>
+                            </VStack>
+                        </HStack>
+                    </Center>
 
-                    <ModalBody bgColor={"$white"}>
+                    <Center mt={10}>
+                        <Text color="black" fontSize={25} mb={5}>Base Stats</Text>
 
-                        <Text color="$black" fontSize={30}>#{poke.id}</Text>
-                        <Text>Altura: {(poke.height)/10} M</Text>       
-                        <Text>Peso: {(poke.weight)/10} kg</Text>         
+                        <Center>
+                            {poke.stats.map((stat: object, index) => {
+                                let statName = stat.stat.name
+                                let baseStat = stat.base_stat
 
-                        <Box mt={10}>
-                            <Text>Type:</Text>
-                            <HStack mt={5}>
-                                    {poke.types.map((type: object, index) => { 
-                                        const tipo = type.type.name;
-                                        const bgTipo = typeColors[tipo] || 'gray';
-                                        return(
-                                            <Center key={index} w={80} borderRadius={10}  ml={3} bgColor={bgTipo}>
-                                                <Text  padding={10} color="white" fontWeight="$bold" >
-                                                    {tipo}
-                                                </Text>
-                                            </Center>
-                                        )
-                                        }
-                                    )}
-                            </HStack>
-                        </Box>  
+                                switch(statName){
+                                    case 'hp':
+                                        statName = 'HP'
+                                        break;
+                                    case 'attack' :
+                                        statName = 'ATK'
+                                        break;
+                                    case 'defense' :
+                                        statName = 'DEF'
+                                        break;
+                                    case 'special-attack' :
+                                        statName = 'SP.ATK'
+                                        break;
+                                    case 'special-defense' :
+                                        statName = 'SP.DEF'
+                                        break;
+                                    case 'speed' :
+                                        statName = 'SPD'
+                                        break;
+                                }
+
+                                const BarUncolor = 300-baseStat;
+                                const barColor = statsColor[statName] || 'black'
+
+                                return(
+                                    <HStack gap={20} mb={10} key={index}>
+                                        <Text left={0}>{statName}</Text>
+
+                                        <HStack w={300} right={0} bgColor="white" borderRadius={10}>
+                                            <Box w={baseStat} bgColor={barColor} alignItems="flex-end" justifyContent="center" borderRadius={10} >
+                                                 {baseStat >= 45 ? <Text right={2} fontSize={12} color="white">{baseStat}/300</Text> : ''}
+                                            </Box>
+                                            <Box w={BarUncolor} bgColor="white" justifyContent="center" alignItems="flex-start" borderTopRightRadius={10} borderBottomRightRadius={10}>
+                                            {baseStat < 45 ? <Text left={2} fontSize={12} color="black">{baseStat}/300</Text> : ''}
+                                            </Box>
+                                        </HStack>
+                                        
+                                            
+                            
+                                    </HStack>
+                                )
+                                
+                            })}
+                        </Center>
+                    </Center>
+
+                       
                         
-                        <Box mt={10}>
+                        <Center mt={10}>
                             <Text color="black">Cheque o pokemon shiny</Text>
                             <TouchableOpacity  onPress={() => setShiny(prevShiny => !prevShiny)} >
                                 <Center mt={5} w={80} bgColor={shiny ? 'black' : '$warmGray300'} borderColor={shiny ? '$warmGray300' : 'black'} borderWidth={2} borderRadius={10}>  
                                     <Text padding={10} color={shiny ? '$warmGray300' : 'black'}>{shiny ? 'Shiny' : 'Normal'}</Text>
                                 </Center>
                             </TouchableOpacity>
-                        </Box>
+                        </Center>
 
-                    </ModalBody>
+                </ModalBody>
                 
 
                 </ModalContent>
-            </Modal>
+        </Modal>
 
   )
 }
