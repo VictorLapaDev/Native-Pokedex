306d35b85a10064cad21ff232032c21eb9d77016
VictorLapaDev <victorlucaslapa@gmail.com>
2024-11-06 18:34:40 -0300

    fix: arrumando menu style

--- src/components/PokeMenu.tsx
+++ src/components/PokeMenu.tsx
@@ -44,50 +44,50 @@ const PokeMenu = ({selected, setSelected}: props) => {
 
               <ModalBody>
                 <VStack>
-                    <RadioGroup value={selected} onChange={setSelected} >
+                    <RadioGroup value={selected} onChange={setSelected}>
 
                         <Radio value="limit=151&offset=0" size="md" >                            
-                            <RadioLabel h={30} color={"$black"} fontWeight={selected === 'limit=151&offset=0' ? '$bold' : '$light'} backgroundColor={selected === 'limit=151&offset=0' ? '$coolGray200' : '$warmGray100'} w={300} >1º Gen</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5} color={"$black"} fontWeight={selected === 'limit=151&offset=0' ? '$bold' : '$light'} backgroundColor={selected === 'limit=151&offset=0' ? '$coolGray200' : '$warmGray100'} w={300} >1º Gen</RadioLabel>
                         </Radio>
                         <Divider/>
                         <Radio value="limit=100&offset=151" size="md" >                            
-                            <RadioLabel h={30}  color={"$black"} fontWeight={selected === 'limit=100&offset=151' ? '$bold' : '$light'} backgroundColor={selected === 'limit=100&offset=151' ? '$coolGray200' : '$warmGray100'} w={300}  >2º Gen</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5}  color={"$black"} fontWeight={selected === 'limit=100&offset=151' ? '$bold' : '$light'} backgroundColor={selected === 'limit=100&offset=151' ? '$coolGray200' : '$warmGray100'} w={300}  >2º Gen</RadioLabel>
                         </Radio>
-                        <Divider/>
+                        <Divider />
                         <Radio value="limit=135&offset=251" size="md">                            
-                            <RadioLabel h={30}  color={"$black"} fontWeight={selected === 'limit=135&offset=251' ? '$bold' : '$light'} backgroundColor={selected === 'limit=135&offset=251' ? '$coolGray200' : '$warmGray100'} w={300} >3º Gen</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5}  color={"$black"} fontWeight={selected === 'limit=135&offset=251' ? '$bold' : '$light'} backgroundColor={selected === 'limit=135&offset=251' ? '$coolGray200' : '$warmGray100'} w={300} >3º Gen</RadioLabel>
                         </Radio>
-                        <Divider/>
+                        <Divider />
                         <Radio value="limit=107&offset=386" size="md">                            
-                            <RadioLabel h={30}  color={"$black"} fontWeight={selected === 'limit=107&offset=386' ? '$bold' : '$light'} backgroundColor={selected === 'limit=107&offset=386' ? '$coolGray200' : '$warmGray100'} w={300} >4º Gen</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5}  color={"$black"} fontWeight={selected === 'limit=107&offset=386' ? '$bold' : '$light'} backgroundColor={selected === 'limit=107&offset=386' ? '$coolGray200' : '$warmGray100'} w={300} >4º Gen</RadioLabel>
                         </Radio>
-                        <Divider/>
+                        <Divider />
                         <Radio value="limit=156&offset=493" size="md">                            
-                            <RadioLabel h={30} color={"$black"} fontWeight={selected === 'limit=156&offset=493' ? '$bold' : '$light'} backgroundColor={selected === 'limit=156&offset=493' ? '$coolGray200' : '$warmGray100'} w={300} >5º Gen</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5} color={"$black"} fontWeight={selected === 'limit=156&offset=493' ? '$bold' : '$light'} backgroundColor={selected === 'limit=156&offset=493' ? '$coolGray200' : '$warmGray100'} w={300} >5º Gen</RadioLabel>
                         </Radio>
-                        <Divider/>
+                        <Divider />
                         <Radio value="limit=72&offset=649" size="md">                            
-                            <RadioLabel h={30} color={"$black"} fontWeight={selected === 'limit=72&offset=649' ? '$bold' : '$light'} backgroundColor={selected === 'limit=72&offset=649' ? '$coolGray200' : '$warmGray100'} w={300} >6º Gen</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5} color={"$black"} fontWeight={selected === 'limit=72&offset=649' ? '$bold' : '$light'} backgroundColor={selected === 'limit=72&offset=649' ? '$coolGray200' : '$warmGray100'} w={300} >6º Gen</RadioLabel>
                         </Radio>
                         <Divider/>
                         <Radio value="limit=88&offset=721" size="md">                            
-                            <RadioLabel h={30} color={"$black"} fontWeight={selected === 'limit=88&offset=721' ? '$bold' : '$light'} backgroundColor={selected === 'limit=88&offset=721' ? '$coolGray200' : '$warmGray100'} w={300} >7º Gen</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5} color={"$black"} fontWeight={selected === 'limit=88&offset=721' ? '$bold' : '$light'} backgroundColor={selected === 'limit=88&offset=721' ? '$coolGray200' : '$warmGray100'} w={300} >7º Gen</RadioLabel>
                         </Radio>
                         <Divider/>
                         <Radio value="limit=96&offset=809" size="md">                            
-                            <RadioLabel h={30} color={"$black"} fontWeight={selected === 'limit=96&offset=809' ? '$bold' : '$light'} backgroundColor={selected === 'limit=96&offset=809' ? '$coolGray200' : '$warmGray100'} w={300} >8º Gen</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5} color={"$black"} fontWeight={selected === 'limit=96&offset=809' ? '$bold' : '$light'} backgroundColor={selected === 'limit=96&offset=809' ? '$coolGray200' : '$warmGray100'} w={300} >8º Gen</RadioLabel>
                         </Radio>
                         <Divider/>   
                         <Radio value="limit=120&offset=905" size="md">                            
-                            <RadioLabel h={30} color={"$black"} fontWeight={selected === 'limit=120&offset=905' ? '$bold' : '$light'} backgroundColor={selected === 'limit=120&offset=905' ? '$coolGray200' : '$warmGray100'} w={300} >9º Gen</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5} color={"$black"} fontWeight={selected === 'limit=120&offset=905' ? '$bold' : '$light'} backgroundColor={selected === 'limit=120&offset=905' ? '$coolGray200' : '$warmGray100'} w={300} >9º Gen</RadioLabel>
                         </Radio>
                         <Divider/>
                         <Radio value="limit=277&offset=1025" size="md">                            
-                            <RadioLabel h={30}  color={"$black"} fontWeight={selected === 'limit=277&offset=1025' ? '$bold' : '$light'} backgroundColor={selected === 'limit=277&offset=1025' ? '$coolGray200' : '$warmGray100'} w={300} >Variados</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5}  color={"$black"} fontWeight={selected === 'limit=277&offset=1025' ? '$bold' : '$light'} backgroundColor={selected === 'limit=277&offset=1025' ? '$coolGray200' : '$warmGray100'} w={300} >Variados</RadioLabel>
                         </Radio>
                         <Divider/>
                         <Radio value="limit=10000" size="md">                            
-                            <RadioLabel h={30} color={"$black"} fontWeight={selected === 'limit=10000' ? '$bold' : '$light'} backgroundColor={selected === 'limit=10000' ? '$coolGray200' : '$warmGray100'} w={300} >Todos</RadioLabel>
+                            <RadioLabel paddingTop={5} paddingBottom={5} color={"$black"} fontWeight={selected === 'limit=10000' ? '$bold' : '$light'} backgroundColor={selected === 'limit=10000' ? '$coolGray200' : '$warmGray100'} w={300} >Todos</RadioLabel>
                         </Radio>
 
                     </RadioGroup>
